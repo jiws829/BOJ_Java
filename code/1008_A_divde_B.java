d27b9f994da6b24d434d52a3e4aae8f956b8f556
jiws829 <jiws829@naver.com>
2023-05-30 20:59:15 +0900

    add 2023.05.30

---
+++ code/1008_A_divde_B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        double answer;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        answer = (double)a / b;
+        System.out.println(answer);
+    }
+}
